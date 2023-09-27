c8f40f2ce804a73f7f6c2b7e7f9f5ae1928c90db
birdkron <js8215001@gmail.com>
2023-09-27 16:31:28 -0600

    Agregada funciones extra

--- Assets/Scripts/Lanzamiento.cs
+++ Assets/Scripts/Lanzamiento.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Lanzamiento : MonoBehaviour
 {
@@ -8,19 +10,35 @@ public class Lanzamiento : MonoBehaviour
     private GameObject[] doroMunicion = null;
     [SerializeField]
     private GameObject puntoSpawn;
+    [SerializeField]
+    private Button btnLanzar;
     // Start is called before the first frame update
     void Start()
     {
-        
+        AgregarEventos(btnLanzar, EventTriggerType.PointerEnter);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            int indice = Random.Range(0,doroMunicion.Length);
-            Instantiate(doroMunicion[indice],puntoSpawn.transform);
+            GenerarDoro();
         }
+    }
+
+    public void GenerarDoro()
+    {
+        int indice = Random.Range(0, doroMunicion.Length);
+        Instantiate(doroMunicion[indice], puntoSpawn.transform);
+    }
+
+    private void AgregarEventos(Button btn, EventTriggerType tipo)
+    {
+        EventTrigger triggerDown = btn.gameObject.AddComponent<EventTrigger>();
+        var pointerDown = new EventTrigger.Entry();
+        pointerDown.eventID = tipo;
+        pointerDown.callback.AddListener((e) => GenerarDoro());
+        triggerDown.triggers.Add(pointerDown);
     }
 }
