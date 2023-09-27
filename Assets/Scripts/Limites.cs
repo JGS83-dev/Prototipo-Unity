c7bcd41532f1d2ced78b8f0a66b948dbd2c6bfd1
birdkron <js8215001@gmail.com>
2023-09-27 13:50:21 -0600

    Script para limite de objeto

---
+++ Assets/Scripts/Limites.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Limites : MonoBehaviour
+{
+    [SerializeField]
+    private float limiteX = -30.0f;
+    [SerializeField]
+    private float limitY = 30.0f;
+    [SerializeField]
+    private float limitZ = 60.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
